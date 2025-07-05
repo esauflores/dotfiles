16ac095f9e5ce5ec195c162e4a4f1e9d7bc5fb74
Cesar Flores <106702032+esauflores@users.noreply.github.com>
2025-07-05 22:49:26 +0700

    Fix shell detection in bling.sh to use script name instead of shell variable

--- bling/bling.sh
+++ bling/bling.sh
@@ -28,7 +28,7 @@ HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-/home/linuxbrew/.linuxbrew}"
 # Atuin allows these flags: "--disable-up-arrow" and/or "--disable-ctrl-r"
 ATUIN_INIT_FLAGS=${ATUIN_INIT_FLAGS:-""}
 
-if [ "$(basename "$SHELL")" = "bash" ]; then
+if [ "$(basename "$0")" = "bash" ]; then
     [ -f "/etc/profile.d/bash-preexec.sh" ] && . "/etc/profile.d/bash-preexec.sh"
     [ -f "/usr/share/bash-prexec" ] && . "/usr/share/bash-prexec"
     [ -f "/usr/share/bash-prexec.sh" ] && . "/usr/share/bash-prexec.sh"
@@ -36,7 +36,7 @@ if [ "$(basename "$SHELL")" = "bash" ]; then
     [ "$(command -v starship)" ] && eval "$(starship init bash)"
     [ "$(command -v atuin)" ] && eval "$(atuin init bash ${ATUIN_INIT_FLAGS})"
     [ "$(command -v zoxide)" ] && eval "$(zoxide init bash)"
-elif [ "$(basename "$SHELL")" = "zsh" ]; then
+elif [ "$(basename "$0")" = "zsh" ]; then
     [ "$(command -v starship)" ] && eval "$(starship init zsh)"
     [ "$(command -v atuin)" ] && eval "$(atuin init zsh ${ATUIN_INIT_FLAGS})"
     [ "$(command -v zoxide)" ] && eval "$(zoxide init zsh)"
